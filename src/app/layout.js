cbcaa4372acc75b0d8fa6c26084ea224cd22fb25
riteshk-007 <codeshorts007@gmail.com>
2026-02-08 17:45:12 +0530

    feat: Add Next.js root layout with global styles, fonts, analytics, and providers.

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,7 +24,29 @@ export const metadata = {
 export default async function RootLayout({ children }) {
   return (
     <html lang="en" dir="ltr">
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NP4R37SX');`}
+        </Script>
+        {/* End Google Tag Manager */}
+      </head>
       <body className={`${raleway.variable} font-sans overflow-x-hidden`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NP4R37SX"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         {/* Google Ads Tag */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=AW-17906869077"

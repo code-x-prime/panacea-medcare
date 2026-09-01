6c510d0c7a1f2922eb9f22dc7e4b2e3be52fd7b7
riteshk-007 <codeshorts007@gmail.com>
2026-09-01 13:53:18 +0530

    feat: add robots.js configuration and robots.txt for search engine indexing

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,10 +1,40 @@
 export default function robots() {
     return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-            disallow: '/admin/',
-        },
+        rules: [
+            {
+                userAgent: '*',
+                allow: '/',
+                disallow: '/admin/',
+            },
+            {
+                userAgent: 'OAI-SearchBot',
+                allow: '/',
+            },
+            {
+                userAgent: 'ChatGPT-User',
+                allow: '/',
+            },
+            {
+                userAgent: 'GPTBot',
+                allow: '/',
+            },
+            {
+                userAgent: 'ClaudeBot',
+                allow: '/',
+            },
+            {
+                userAgent: 'Claude-User',
+                allow: '/',
+            },
+            {
+                userAgent: 'PerplexityBot',
+                allow: '/',
+            },
+            {
+                userAgent: 'Perplexity-User',
+                allow: '/',
+            },
+        ],
         sitemap: 'https://www.panaceamedcare.com/sitemap.xml',
     }
 }
